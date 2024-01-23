91a6f90058596c00e66614a35ce20d912e6d5b1e
Gustavo Ledo <123033640+gugsinwork@users.noreply.github.com>
2024-01-23 17:46:54 -0300

    UPDATE: internet

---
+++ public/script.js
@@ -0,0 +1,11 @@
+function redirecionarParaResidencia() {
+    window.location.href = 'residencia.html';
+};
+function redirecionarParaViagem() {
+    window.location.href = 'viagem.html';
+};
+function redirecionarParaBarco() {
+    window.location.href = 'barco.html';
+};
+
+
